1b392f754948ea0267a8b916c60309d0c114c1dd
Sergey Chystiakov <anyxem@gmail.com>
2022-05-30 10:31:05 +0200

    android release

---
+++ update_nodemodules.sh
@@ -0,0 +1,4 @@
+echo "Updating react-native-tor"
+cp core/react-native-tor/TorPackage.kt node_modules/react-native-tor/android/src/main/java/com/reactnativetor/TorPackage.kt
+
+echo "node_modules successfully updated"
