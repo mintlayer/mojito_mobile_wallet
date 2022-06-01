fbfe4c8267cb6b58f18d2e31984597b8165be1c7
zainali.syed24 <zainali.syed24@hotmail.com>
2022-06-02 03:11:07 +0500

    slider issue fix

--- screen/introduction/index.js
+++ screen/introduction/index.js
@@ -14,57 +14,40 @@ const Introduction = () => {
   const [activePage, setActivePage] = useState(0);
   const _renderItem = ({ item }) => {
     const { image, title, subtitle } = item;
-    return <View style={styles.slide1}>{pageContent(image, title, subtitle)}</View>;
+    return (
+      <View style={styles.slide1}>
+        <Image source={image} />
+        <Text style={styles.text}>{title}</Text>
+        <Text style={styles.description}>{subtitle}</Text>
+      </View>
+    );
   };
+
+  const { navigate } = useNavigation();
+  const navigateScreen = (navigate) => navigate('BottomTab');
 
   return (
     <View style={styles.root}>
-      <Carousel
-        data={introductionContent}
-        renderItem={_renderItem}
-        onSnapToItem={(index) => {
-          setActivePage(index);
-        }}
-        useScrollView
-        sliderWidth={windowWidth}
-        itemWidth={windowWidth}
-        inactiveSlideScale={0.94}
-        inactiveSlideOpacity={0.7}
-      />
+      <Carousel data={introductionContent} renderItem={_renderItem} sliderWidth={windowWidth} itemWidth={windowWidth} onSnapToItem={(index) => setActivePage(index)} enableMomentum decelerationRate={0.9} />
       <Pagination dotsLength={introductionContent.length} activeDotIndex={activePage} dotStyle={styles.activeDot} inactiveDotStyle={styles.inActiveDot} />
 
-      <View style={styles.skipButtonPortion}>{activePage === 3 ? StartButton() : Skip()}</View>
+      <View style={styles.skipButtonPortion}>
+        {activePage === 3 ? (
+          <TouchableOpacity style={styles.startButton} onPress={() => navigateScreen(navigate)}>
+            <Text style={styles.startButtonText}>Start</Text>
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity style={styles.skipButton} onPress={() => navigateScreen(navigate)}>
+            <Text style={styles.skipButtonText}>Skip</Text>
+          </TouchableOpacity>
+        )}
+      </View>
     </View>
   );
 };
-// const navigateScreen = (navigate) => navigate('WalletsList');
-const navigateScreen = (navigate) => navigate('BottomTab');
-
-const Skip = () => {
-  const { navigate } = useNavigation();
-  return (
-    <TouchableOpacity style={styles.skipButton} onPress={() => navigateScreen(navigate)}>
-      <Text style={styles.skipButtonText}>Skip</Text>
-    </TouchableOpacity>
-  );
-};
-const StartButton = () => {
-  const { navigate } = useNavigation();
-  return (
-    <TouchableOpacity style={styles.startButton} onPress={() => navigateScreen(navigate)}>
-      <Text style={styles.startButtonText}>Start</Text>
-    </TouchableOpacity>
-  );
-};
-const pageContent = (image, title, subTitle) => (
-  <React.Fragment key={title}>
-    <Image source={image} />
-    <Text style={styles.text}>{title}</Text>
-    <Text style={styles.description}>{subTitle}</Text>
-  </React.Fragment>
-);
 
 export default Introduction;
+
 Introduction.navigationOptions = navigationStyle({}, (opts) => ({
   ...opts,
   headerTitle: '',
@@ -75,14 +58,14 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   slide1: {
-    flex: 1,
+    width: windowWidth,
     alignItems: 'center',
-    marginHorizontal: 30,
+    paddingHorizontal: 30,
   },
   text: {
     color: COLORS.black,
     fontSize: 18,
-    marginVertical: 30,
+    marginVertical: 20,
     fontFamily: type.bold,
   },
   description: {
@@ -126,7 +109,6 @@ const styles = StyleSheet.create({
     fontSize: 14,
   },
   skipButtonPortion: {
-    flex: 0.18,
     alignItems: 'center',
   },
 });
