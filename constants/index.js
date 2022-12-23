705619aae95d3d35394e97fb927381a4bc12e866
Alexander <69318224+owlsua@users.noreply.github.com>
2022-12-23 13:50:10 +0100

    A-1202579766983037: adds a validation for the amount field (#82)
    * add amount field validation
    
    * fix names, regex and error message
    
    * remove env
    
    * package json lock file
    
    Co-authored-by: zainali.syed24 <zainali.syed24@hotmail.com>

--- constants/index.js
+++ constants/index.js
@@ -27,3 +27,7 @@ export const introductionContent = [
 ];
 
 export const androidLevel = 28;
+
+export const amountValueRegex = `^[1-9]\\d*(\\,\\d+)?$`;
+
+export const recieveDescriptionRegex = `^[\\d@a-zA-Z._+-\\s]+$`;
