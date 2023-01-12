f3e335361dbeb16d0378787d8db51adfeddcaf2f
zainali.syed24 <zainali.syed24@hotmail.com>
2023-01-12 20:41:56 +0500

    trans issue and env setup

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,23 @@
+// module.exports = {
+//   presets: ['module:metro-react-native-babel-preset'],
+//   plugins: ['react-native-reanimated/plugin'], // required by react-native-reanimated v2 https://docs.swmansion.com/react-native-reanimated/docs/installation/
+// };
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'], // required by react-native-reanimated v2 https://docs.swmansion.com/react-native-reanimated/docs/installation/
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        allowlist: ['MINI_AMOUNT'],
+        blacklist: null,
+        whitelist: null,
+        safe: true,
+        allowUndefined: true,
+      },
+    ],
+    ['react-native-reanimated/plugin'], // required by react-native-reanimated v2 https://docs.swmansion.com/react-native-reanimated/docs/installation/
+  ],
 };
