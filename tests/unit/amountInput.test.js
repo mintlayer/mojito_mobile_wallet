c3f30173854a283109456f70407e496579f10964
owlsua <allatsnow@gmail.com>
2023-02-01 08:41:46 +0100

    adds additional test cases

--- tests/unit/amountInput.test.js
+++ tests/unit/amountInput.test.js
@@ -2,19 +2,37 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import AmountInput from '../../components/AmountInput';
 
+const amount = 0.00000012;
 const scientificAmount = 1.2e-7;
 const expectedAmount = '0.00000012';
 const mockFn = jest.fn();
 
-const PROPS_MOCK = {
+const PROPS_MOCK_01 = {
+  amount: amount,
+  onChangeText: mockFn,
+  onAmountUnitChange: mockFn,
+};
+
+const PROPS_MOCK_02 = {
   amount: scientificAmount,
   onChangeText: mockFn,
   onAmountUnitChange: mockFn,
 };
 
-test('renders AmountInput with Scientific Notation value', () => {
-  const component = renderer.create(<AmountInput {...PROPS_MOCK} />);
+test('renders AmountInput', () => {
+  const component = renderer.create(<AmountInput {...PROPS_MOCK_01} />);
   const textInput = component.root.findByType('TextInput');
+
+  jest.useFakeTimers();
+
+  expect(textInput.props.value).toBe(expectedAmount);
+});
+
+test('renders AmountInput with Scientific Notation value', () => {
+  const component = renderer.create(<AmountInput {...PROPS_MOCK_02} />);
+  const textInput = component.root.findByType('TextInput');
+
+  jest.useFakeTimers();
 
   expect(textInput.props.value).toBe(expectedAmount);
 });
