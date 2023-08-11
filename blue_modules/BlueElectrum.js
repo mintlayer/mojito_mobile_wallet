48438b819376232a7cb1aaee149a3b5b67a44ee4
Renie Siqueira <renie.webdev@gmail.com>
2023-08-11 20:47:26 +0300

    chore: Improving docs. chore: fixing duplicated variable.

--- blue_modules/BlueElectrum.js
+++ blue_modules/BlueElectrum.js
@@ -67,12 +67,6 @@ const hardcodedTestnetPeers = [
   { host: 'electrum.blockstream.info', ssl: '60002' },
 ];
 
-const hardcodedTestnetPeers = [
-  { host: 'testnet.hsmiths.com', ssl: '53012' },
-  { host: 'testnet.aranguren.org', ssl: '51002' },
-  { host: 'electrum.blockstream.info', ssl: '60002' },
-];
-
 /** @type {ElectrumClient} */
 let mainClient;
 let mainConnected = false;
